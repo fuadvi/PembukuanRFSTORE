b90d46360ce32b93ec57205c75a8ff5f0fbc7981
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-02-21 14:10:02 +0700

    membuat table modals

---
+++ database/migrations/2021_02_21_070852_create_modals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('modal_bulanan');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modals');
+    }
+}
