04a63ca767f7cf9f016377ea9f8a087005d61b34
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-02-21 14:12:45 +0700

    membuat table pengeluaran

---
+++ database/migrations/2021_02_21_071053_create_pengeluaran_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengeluaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengeluaran', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_produk');
+            $table->string('description')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('price');
+            $table->date('purchase_ date');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengeluaran');
+    }
+}
