e7a1915b1fd566020682f313a50897c81c73cfd9
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-02-26 08:58:39 +0700

    memperbaiki tampilan select create  pemasukan

--- resources/views/pages/pemasukan/create.blade.php
+++ resources/views/pages/pemasukan/create.blade.php
@@ -28,10 +28,15 @@
                         </div>
                         <div class='w-full md:w-full px-3 mb-6'>
                             <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' for='grid-text-1'>Tanggal Pemasukan</label>
-                            <select name="status" class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
-                                <option value="PRIBADI">Status</option>
-                                <option value="KAS">Uang Kas</option>
-                            </select>
+                            <div class="lex-shrink w-full inline-block relative">
+                                <select name="status" class='block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded'>
+                                    <option value="PRIBADI">Uang Pribadi</option>
+                                    <option value="KAS">Uang Kas</option>
+                                </select>
+                                <div class="pointer-events-none absolute top-0 mt-3  right-0 flex items-center px-2 text-gray-600">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="personal w-full border-t pt-2">
