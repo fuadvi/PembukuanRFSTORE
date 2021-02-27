2a40e41cf22adf0941a290a9696ee19f3c304341
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-02-27 10:27:26 +0700

    membuat tampilan dasboard menjadi dinamis

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KasScarllate;
+use App\Models\Pemasukan;
+use App\Models\Pengeluaran;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DasboardController extends Controller
 {
     public function index()
     {
-        return view('pages.AdminDasboard');
+        $dataKas = KasScarllate::all();
+        $jumlah_kas = 0;
+        foreach ($dataKas as  $kas) {
+            $jumlah_kas = $kas->uang_kas;
+        }
+
+        return view('pages.AdminDasboard')->with([
+            'pemasukan' => Pemasukan::count(),
+            'JumlahKas' => $jumlah_kas,
+            'pengeluaran' => Pengeluaran::count(),
+            'user' => User::count(),
+        ]);
     }
 }
--- resources/views/pages/AdminDasboard.blade.php
+++ resources/views/pages/AdminDasboard.blade.php
@@ -18,8 +18,38 @@
                             <div class="rounded-full p-5 bg-green-600"><i class="fa fa-wallet fa-2x fa-inverse"></i></div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-600">Total Revenue</h5>
-                            <h3 class="font-bold text-3xl">$3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            <h5 class="font-bold uppercase text-gray-600">Total Pemasukan</h5>
+                            <h3 class="font-bold text-3xl">{{ $pemasukan ? $pemasukan : 0 }} <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                        </div>
+                    </div>
+                </div>
+                <!--/Metric Card-->
+            </div>
+            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                <!--Metric Card-->
+                <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
+                    <div class="flex flex-row items-center">
+                        <div class="flex-shrink pr-4">
+                            <div class="rounded-full p-5 bg-green-600"><i class="fa fa-donate fa-2x fa-inverse"></i></div>
+                        </div>
+                        <div class="flex-1 text-right md:text-center">
+                            <h5 class="font-bold uppercase text-gray-600">Jumlah Kas</h5>
+                            <h3 class="font-bold text-3xl">${{ $JumlahKas ? $JumlahKas : 0 }} <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                        </div>
+                    </div>
+                </div>
+                <!--/Metric Card-->
+            </div>
+            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                <!--Metric Card-->
+                <div class="bg-gradient-to-b from-red-200 to-red-100 border-b-4 border-red-600 rounded-lg shadow-xl p-5">
+                    <div class="flex flex-row items-center">
+                        <div class="flex-shrink pr-4">
+                            <div class="rounded-full p-5 bg-red-600"><i class="fa fa-wallet fa-2x fa-inverse"></i></div>
+                        </div>
+                        <div class="flex-1 text-right md:text-center">
+                            <h5 class="font-bold uppercase text-gray-600">Total Pengeluaran</h5>
+                            <h3 class="font-bold text-3xl">{{ $pengeluaran ? $pengeluaran : 0 }} <span class="text-red-500"><i class="fas fa-caret-down"></i></span></h3>
                         </div>
                     </div>
                 </div>
@@ -34,7 +64,7 @@
                         </div>
                         <div class="flex-1 text-right md:text-center">
                             <h5 class="font-bold uppercase text-gray-600">Total Users</h5>
-                            <h3 class="font-bold text-3xl">249 <span class="text-pink-500"><i class="fas fa-exchange-alt"></i></span></h3>
+                            <h3 class="font-bold text-3xl">{{ $user ? $user : 0 }} <span class="text-pink-500"><i class="fas fa-exchange-alt"></i></span></h3>
                         </div>
                     </div>
                 </div>
@@ -45,56 +75,11 @@
                 <div class="bg-gradient-to-b from-yellow-200 to-yellow-100 border-b-4 border-yellow-600 rounded-lg shadow-xl p-5">
                     <div class="flex flex-row items-center">
                         <div class="flex-shrink pr-4">
-                            <div class="rounded-full p-5 bg-yellow-600"><i class="fas fa-user-plus fa-2x fa-inverse"></i></div>
+                            <div class="rounded-full p-5 bg-yellow-600"><i class="fas fa-coins fa-2x fa-inverse"></i></div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-600">New Users</h5>
+                            <h5 class="font-bold uppercase text-gray-600">Modal</h5>
                             <h3 class="font-bold text-3xl">2 <span class="text-yellow-600"><i class="fas fa-caret-up"></i></span></h3>
-                        </div>
-                    </div>
-                </div>
-                <!--/Metric Card-->
-            </div>
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Metric Card-->
-                <div class="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4 border-blue-500 rounded-lg shadow-xl p-5">
-                    <div class="flex flex-row items-center">
-                        <div class="flex-shrink pr-4">
-                            <div class="rounded-full p-5 bg-blue-600"><i class="fas fa-server fa-2x fa-inverse"></i></div>
-                        </div>
-                        <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-600">Server Uptime</h5>
-                            <h3 class="font-bold text-3xl">152 days</h3>
-                        </div>
-                    </div>
-                </div>
-                <!--/Metric Card-->
-            </div>
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Metric Card-->
-                <div class="bg-gradient-to-b from-indigo-200 to-indigo-100 border-b-4 border-indigo-500 rounded-lg shadow-xl p-5">
-                    <div class="flex flex-row items-center">
-                        <div class="flex-shrink pr-4">
-                            <div class="rounded-full p-5 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-inverse"></i></div>
-                        </div>
-                        <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-600">To Do List</h5>
-                            <h3 class="font-bold text-3xl">7 tasks</h3>
-                        </div>
-                    </div>
-                </div>
-                <!--/Metric Card-->
-            </div>
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Metric Card-->
-                <div class="bg-gradient-to-b from-red-200 to-red-100 border-b-4 border-red-500 rounded-lg shadow-xl p-5">
-                    <div class="flex flex-row items-center">
-                        <div class="flex-shrink pr-4">
-                            <div class="rounded-full p-5 bg-red-600"><i class="fas fa-inbox fa-2x fa-inverse"></i></div>
-                        </div>
-                        <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-600">Issues</h5>
-                            <h3 class="font-bold text-3xl">3 <span class="text-red-500"><i class="fas fa-caret-up"></i></span></h3>
                         </div>
                     </div>
                 </div>
